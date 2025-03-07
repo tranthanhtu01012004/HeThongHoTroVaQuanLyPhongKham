8d8f489426d4bb06a0a4529fc68ddb9436e40c7c
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2025-03-07 18:08:59 +0700

    feat: Them TaiKhoanController va lop ApiResponse
    - Them lop ApiResponse de dinh dang du lieu tra ve theo chuan, tang tinh tai su dung
    - Them TaiKhoanContoler bao gom cac hanh dong CRUD.

---
+++ Server/HeThongHoTroVaQuanLyPhongKham/Common/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace HeThongHoTroVaQuanLyPhongKham.Common
+{
+    public class ApiResponse<TDto> where TDto : class
+    {
+        public bool Status { get; set; }
+        public string? Message { get; set; }
+        public TDto? Data { get; set; }
+
+        public ApiResponse(bool Status, string? Message, TDto? data)
+        {
+            this.Status = Status;
+            this.Message = Message;
+            Data = data;
+        }
+
+        public static ApiResponse<TDto> Success(TDto? Data, string? message = "Lấy dữ liệu thành công.")
+        {
+            return new ApiResponse<TDto>(true, message, Data);
+        }
+
+        public static ApiResponse<TDto> Fail(string message)
+        {
+            return new ApiResponse<TDto>(false, message, null);
+        }
+    }
+}
