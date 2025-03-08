e9e8ecf294aba6202385b5014725e8d18b4db873
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2025-03-08 16:32:13 +0700

    chore: thay doi vi tri thu muc cua Custom Exception
    - Thay vi luu tru Custom Exceptions trong thu muc Common thi luu tru vao thu muc Exception de tang tinh nhat quan.

---
+++ Server/HeThongHoTroVaQuanLyPhongKham/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,7 @@
+﻿namespace HeThongHoTroVaQuanLyPhongKham.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message) : base(message) { }
+    }
+}
