bafb64f00dc52432d9b210b27bebc93fa8e6e4a2
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2025-03-18 17:43:25 +0700

    feat(thuoc): thực hiện các chức năng quản lý thuốc và chỉnh sửa directive để cho phép nhiều quyền cùng thực thi.

--- Client/HeThongHoTroVaQuanLyPhongKham/src/app/app.routes.ts
+++ Client/HeThongHoTroVaQuanLyPhongKham/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { UnauthorizedComponent } from './users/components/unauthorized/unauthori
 import { authGuard } from './guards/authGuards';
 import { NhanVienComponent } from './admins/components/nhan-vien/nhan-vien.component';
 import { QuanLyLichHenComponent } from './admins/components/quan-ly-lich-hen/quan-ly-lich-hen.component';
+import { ThuocComponent } from './admins/components/thuoc/thuoc.component';
 
 export const routes: Routes = [
   { 
@@ -38,7 +39,8 @@ export const routes: Routes = [
       { path: 'phong-kham', component: PhongKhamComponent },
       { path: 'dich-vu-y-te', component: DichVuYTeComponent },
       { path: 'nhan-vien', component: NhanVienComponent },
-      { path: 'lich-hen', component: QuanLyLichHenComponent }
+      { path: 'lich-hen', component: QuanLyLichHenComponent },
+      { path: 'thuoc', component: ThuocComponent }
 
     ]
   }
---
+++ Client/HeThongHoTroVaQuanLyPhongKham/src/app/interfaces/thuoc/IThuoc.ts
@@ -0,0 +1,9 @@
+export interface IThuoc {
+    maThuoc: number;
+    ten: string;
+    moTa: string;
+    donVi: string;
+    chongChiDinh: string;
+    tuongTacThuoc: string
+    donGia: number;
+}
